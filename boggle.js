45b11d3220280172e3068c15923c0235dc6c9be5
fastabiqul1993 <fastabiqul1993@gmail.com>
2018-04-09 06:33:32 +0700

    done

--- boggle.js
+++ boggle.js
@@ -1,46 +1,175 @@
-function generateBoard() {
-    
-    let chars = 'abcdefghijklmnopqrstuvwxyz'
-    let outsideBox =[]
+const listWord = require('./data.js')
 
-    for (let row = 0; row < 4; row++) {
-        
-        let insideBox = []
-        for (let col = 0; col < 4; col++) {
-            
-            let randomChar = Math.floor(Math.random()*(chars.length-1))
-            insideBox.push(chars[randomChar])
-        }
+class Boggle {
 
-        outsideBox.push(insideBox)
+    constructor() {
+
+        this.board = []
+        this.words = listWord
     }
 
-    return outsideBox
-}
+    generateBoard(num) {
 
-function scanBoard() {
-    
-    let board = [ 
-                    [ 'D', 'G', 'H', 'I' ],
-                    [ 'K', 'L', 'P', 'S' ],
-                    [ 'Y', 'E', 'U', 'T' ],
-                    [ 'E', 'O', 'R', 'N' ] 
-                ]
-  
-    let word = 'TURN'            
-    let cordinatTarget = []
-    
-    for (let row = 0; row < board.length; row++) {
-        
-        for (let col = 0; col < board[col].length; col++) {
+        let abjad = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+        let boardGame = this.board
+
+        for (let i = 0; i < num; i++) {
+            boardGame.push([])
+
+            for (let j = 0; j < num; j++) {
+                let index = Math.floor(Math.random() * 26)
+                boardGame[i].push(abjad[index])
+                
+            }
             
-            if(board[i][j] === word[0]) {
+        }
 
+        return boardGame
+    }
+
+    getFirstLetter(index) {
+
+        let findLetter = this.words[0]
+        let firstCoordinat = []
+        let boardGame = this.board
+
+        for (let i = 0; i < boardGame.length; i++) {
+            
+            for (let j = 0; j < boardGame.length; j++) {
+                if(boardGame[i][j] === findLetter) {
+
+                    firstCoordinat.push([i,j])
+                }
                 
             }
         }
+
+        return firstCoordinat
     }
-    
+
+    checkVisitCoor(arr) {
+
+        let count = 0
+
+        for (let i = 0; i < arr.length; i++) {
+            for (let j = 0; j < arr.length; j++) {
+                if(arr[i][j] === 1) {
+
+                    count++
+                }
+                
+            }
+            
+        }
+
+        return count
+    }
+
+    checkBlock(row, col, x, y) {
+
+        let limitRow = [row-1, row, row+1]
+        let limitCol = [col-1, col, col+1]
+        let checkRow = limitRow.indexOf(x)
+        let checkCol = limitCol.indexOf(y)
+
+        if (checkRow !== -1 && checkCol !== -1) {
+
+            return true
+        } else {
+
+            return false
+        }
+    }
+
+    checkVisit(){
+
+        let visited = []
+        let boardGame = this.board
+
+        for (let i = 0; i < boardGame.length; i++) {
+            visited.push([])
+            
+            for (let j = 0; j < boardGame[i].length; j++) {
+                
+                visited[i].push(0)
+            }
+        }
+
+        return visited
+    }
+
+    solve() {
+
+        let result = []
+        let boardGame = this.board
+        let findWord = this.words
+        let counter = 0
+
+        for (let i = 0; i < findWord.length; i++) {
+            
+            let kemungkinan = this.getFirstLetter(i)
+
+            for (let j = 0; j < kemungkinan.length; j++) {
+                
+                let statusWord = false
+                let visited = this.checkVisit()
+                let row = kemungkinan[j][0]
+                let col = kemungkinan[j][1]
+
+                visited[row][col] =1
+                let lastPos = [row,col]
+
+                for (let k = 1; k < findWord[i].length; k++) {
+                    
+                    let status = false
+
+                    for (let l = 0; l < boardGame.length; l++) {
+                        for (let m = 0; m < boardGame[l].length; m++) {
+                            if(findWord[i][k] === boardGame[l][m]) {
+
+                                if(this.checkBlock(lastPos[0],lastPos[1],l,m)) {
+
+                                    if(visited[l][m] === 0) {
+
+                                        lastPos = [l,m]
+                                        visited[l,m] = 1
+                                        status = true
+                                        break
+                                    }
+                                }
+                            }
+                            
+                        }
+                        
+                        if(status === true) {
+
+                            break
+                        }
+                    }
+                }
+            }
+
+            let countVisit = this.checkVisitCoor(visited)
+            if(countVisit === kemungkinan.length) {
+
+                result.push(findWord[i])
+                counter++
+                statusWord = true
+            }
+            if(statusWord === true) {
+
+                break
+            }
+        }
+
+
+    return {counter, result}
+    }
 }
 
-scanBoard()
+let boggle = new Boggle()
+boggle.generateBoard(4)
+console.log((boggle.generateBoard));
+console.log('Words Found = ',boggle.solve().counter);
+console.log('Words List = ', boggle.solve().result);
+
