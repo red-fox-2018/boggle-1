ae410f2c4627f27021ad6ca420227f2ca0c3b564
restuu <restu_u@outlook.com>
2018-04-08 17:41:12 +0700

    add checkIndex(), but the code is still too long

--- boggle.js
+++ boggle.js
@@ -1,12 +1,6 @@
 class Boggle {
   constructor() {
-    this.board = [
-                  [ 'B', 'B', 'F', 'D' ],
-                  [ 'B', 'O', 'K', 'M' ],
-                  [ 'H', 'Y', 'R', 'U' ],
-                  [ 'O', 'E', 'J', 'K' ],
-                 ];
-    this.word = 'FORK';
+    this.board = [];
     this.copyBoard = [];
   }
 
@@ -29,115 +23,166 @@ class Boggle {
     return String.fromCharCode(letterCode);
   }
 
-  checkWord() {
-    let word = this.word.toUpperCase();
+  checkWord(word) {
+    word = word.toUpperCase();
     for (var i = 0; i < this.board.length; i++) {
       for (var j = 0; j < this.board[i].length; j++) {
         let indexWord = 0;
+        let str = '';
         if (this.board[i][j] === word[indexWord]) {
+          str += word[indexWord];
           let wordObj = {};
           wordObj[word[indexWord]] = [i, j];
           indexWord++;
           let selectedIdx_i = i;
           let selectedIdx_j = j;
           let limitLoop = 8
-          while (indexWord < word.length || limitLoop >= 0) {
+          while (indexWord < word.length && limitLoop >= 0) {
             debugger
             if ((selectedIdx_i - 1) < 0) {
-              if (this.board[selectedIdx_i][selectedIdx_j + 1] === word[indexWord]) { //right
+              if (this.board[selectedIdx_i][selectedIdx_j + 1] === word[indexWord] && !this.checkIndex(selectedIdx_i, selectedIdx_j + 1, wordObj)) { //right
                 selectedIdx_j++;
                 wordObj[word[indexWord]] = [selectedIdx_i, selectedIdx_j];
+                str += word[indexWord];
                 indexWord++;
-              } else if (this.board[selectedIdx_i][selectedIdx_j - 1] === word[indexWord]) { //left
+              } else if (this.board[selectedIdx_i][selectedIdx_j - 1] === word[indexWord] && !this.checkIndex(selectedIdx_i, selectedIdx_j - 1, wordObj)) { //left
                 selectedIdx_j--;
                 wordObj[word[indexWord]] = [selectedIdx_i, selectedIdx_j];
+                str += word[indexWord];
                 indexWord++;
-              } else if (this.board[selectedIdx_i + 1][selectedIdx_j] === word[indexWord]) { //down
+              } else if (this.board[selectedIdx_i + 1][selectedIdx_j] === word[indexWord] && !this.checkIndex(selectedIdx_i + 1, selectedIdx_j, wordObj)) { //down
                 selectedIdx_i++;
                 wordObj[word[indexWord]] = [selectedIdx_i, selectedIdx_j];
+                str += word[indexWord];
                 indexWord++;
-              } else if (this.board[selectedIdx_i + 1][selectedIdx_j + 1] === word[indexWord]) { //bottom-right
+              } else if (this.board[selectedIdx_i + 1][selectedIdx_j + 1] === word[indexWord] && !this.checkIndex(selectedIdx_i + 1, selectedIdx_j + 1, wordObj)) { //bottom-right
                 selectedIdx_i++;
                 selectedIdx_j++;
                 wordObj[word[indexWord]] = [selectedIdx_i, selectedIdx_j];
+                str += word[indexWord];
                 indexWord++;
-              } else if (this.board[selectedIdx_i + 1][selectedIdx_j - 1] === word[indexWord]) { //bottom-left
+              } else if (this.board[selectedIdx_i + 1][selectedIdx_j - 1] === word[indexWord] && !this.checkIndex(selectedIdx_i + 1, selectedIdx_j - 1, wordObj)) { //bottom-left
                 selectedIdx_i++;
                 selectedIdx_j--;
                 wordObj[word[indexWord]] = [selectedIdx_i, selectedIdx_j];
+                str += word[indexWord];
                 indexWord++;
               }
             } else if ((selectedIdx_i + 1) >= this.board.length) {
-              if (this.board[selectedIdx_i][selectedIdx_j + 1] === word[indexWord]) { //right
+              if (this.board[selectedIdx_i][selectedIdx_j + 1] === word[indexWord] && !this.checkIndex(selectedIdx_i, selectedIdx_j + 1, wordObj)) { //right
                 selectedIdx_j++;
                 wordObj[word[indexWord]] = [selectedIdx_i, selectedIdx_j];
+                str += word[indexWord];
                 indexWord++;
-              } else if (this.board[selectedIdx_i][selectedIdx_j - 1] === word[indexWord]) { //left
+              } else if (this.board[selectedIdx_i][selectedIdx_j - 1] === word[indexWord] && !this.checkIndex(selectedIdx_i, selectedIdx_j - 1, wordObj)) { //left
                 selectedIdx_j--;
                 wordObj[word[indexWord]] = [selectedIdx_i, selectedIdx_j];
+                str += word[indexWord];
                 indexWord++;
-              } else if (this.board[selectedIdx_i - 1][selectedIdx_j] === word[indexWord]) { //up
+              } else if (this.board[selectedIdx_i - 1][selectedIdx_j] === word[indexWord] && !this.checkIndex(selectedIdx_i - 1, selectedIdx_j, wordObj)) { //up
                 selectedIdx_i--;
                 wordObj[word[indexWord]] = [selectedIdx_i, selectedIdx_j];
+                str += word[indexWord];
                 indexWord++;
-              } else if (this.board[selectedIdx_i - 1][selectedIdx_j + 1] === word[indexWord]) { //top-right
+              } else if (this.board[selectedIdx_i - 1][selectedIdx_j + 1] === word[indexWord] && !this.checkIndex(selectedIdx_i - 1, selectedIdx_j + 1, wordObj)) { //top-right
                 selectedIdx_i--;
                 selectedIdx_j++;
                 wordObj[word[indexWord]] = [selectedIdx_i, selectedIdx_j];
+                str += word[indexWord];
                 indexWord++;
-              } else if (this.board[selectedIdx_i - 1][selectedIdx_j - 1] === word[indexWord]) { //top-left
+              } else if (this.board[selectedIdx_i - 1][selectedIdx_j - 1] === word[indexWord] && !this.checkIndex(selectedIdx_i - 1, selectedIdx_j - 1, wordObj)) { //top-left
                 selectedIdx_i--;
                 selectedIdx_j--;
                 wordObj[word[indexWord]] = [selectedIdx_i, selectedIdx_j];
+                str += word[indexWord];
                 indexWord++;
               }
             } else {
-              if (this.board[selectedIdx_i][selectedIdx_j + 1] === word[indexWord]) { //right
+              if (this.board[selectedIdx_i][selectedIdx_j + 1] === word[indexWord] && !this.checkIndex(selectedIdx_i, selectedIdx_j + 1, wordObj)) { //right
                 selectedIdx_j++;
                 wordObj[word[indexWord]] = [selectedIdx_i, selectedIdx_j];
+                str += word[indexWord];
                 indexWord++;
-              } else if (this.board[selectedIdx_i][selectedIdx_j - 1] === word[indexWord]) { //left
+              } else if (this.board[selectedIdx_i][selectedIdx_j - 1] === word[indexWord] && !this.checkIndex(selectedIdx_i, selectedIdx_j - 1, wordObj)) { //left
                 selectedIdx_j--;
                 wordObj[word[indexWord]] = [selectedIdx_i, selectedIdx_j];
+                str += word[indexWord];
                 indexWord++;
-              } else if (this.board[selectedIdx_i - 1][selectedIdx_j] === word[indexWord]) { //up
+              } else if (this.board[selectedIdx_i - 1][selectedIdx_j] === word[indexWord] && !this.checkIndex(selectedIdx_i - 1, selectedIdx_j, wordObj)) { //up
                 selectedIdx_i--;
                 wordObj[word[indexWord]] = [selectedIdx_i, selectedIdx_j];
+                str += word[indexWord];
                 indexWord++;
-              } else if (this.board[selectedIdx_i - 1][selectedIdx_j + 1] === word[indexWord]) { //top-right
+              } else if (this.board[selectedIdx_i - 1][selectedIdx_j + 1] === word[indexWord] && !this.checkIndex(selectedIdx_i - 1, selectedIdx_j + 1, wordObj)) { //top-right
                 selectedIdx_i--;
                 selectedIdx_j++;
                 wordObj[word[indexWord]] = [selectedIdx_i, selectedIdx_j];
+                str += word[indexWord];
                 indexWord++;
-              } else if (this.board[selectedIdx_i - 1][selectedIdx_j - 1] === word[indexWord]) { //top-left
+              } else if (this.board[selectedIdx_i - 1][selectedIdx_j - 1] === word[indexWord] && !this.checkIndex(selectedIdx_i - 1, selectedIdx_j - 1, wordObj)) { //top-left
                 selectedIdx_i--;
                 selectedIdx_j--;
                 wordObj[word[indexWord]] = [selectedIdx_i, selectedIdx_j];
+                str += word[indexWord];
                 indexWord++;
-              } else if (this.board[selectedIdx_i + 1][selectedIdx_j] === word[indexWord]) { //down
+              } else if (this.board[selectedIdx_i + 1][selectedIdx_j] === word[indexWord] && !this.checkIndex(selectedIdx_i + 1, selectedIdx_j, wordObj)) { //down
                 selectedIdx_i++;
                 wordObj[word[indexWord]] = [selectedIdx_i, selectedIdx_j];
+                str += word[indexWord];
                 indexWord++;
-              } else if (this.board[selectedIdx_i + 1][selectedIdx_j + 1] === word[indexWord]) { //bottom-right
+              } else if (this.board[selectedIdx_i + 1][selectedIdx_j + 1] === word[indexWord] && !this.checkIndex(selectedIdx_i + 1, selectedIdx_j + 1, wordObj)) { //bottom-right
                 selectedIdx_i++;
                 selectedIdx_j++;
                 wordObj[word[indexWord]] = [selectedIdx_i, selectedIdx_j];
+                str += word[indexWord];
                 indexWord++;
-              } else if (this.board[selectedIdx_i + 1][selectedIdx_j - 1] === word[indexWord]) { //bottom-left
+              } else if (this.board[selectedIdx_i + 1][selectedIdx_j - 1] === word[indexWord] && !this.checkIndex(selectedIdx_i + 1, selectedIdx_j - 1, wordObj)) { //bottom-left
                 selectedIdx_i++;
                 selectedIdx_j--;
                 wordObj[word[indexWord]] = [selectedIdx_i, selectedIdx_j];
+                str += word[indexWord];
                 indexWord++;
               }
             }
             limitLoop--
           }
           limitLoop = 8;
-          console.log(wordObj);
+          // console.log(wordObj);
+          if (str === word) {
+            return true;
+          }
         }
       }
     }
-    console.log(word);
+    return false;
+  }
+
+  testCase() {
+    let dictionary = [
+                      'FORK', 'APPLE', 'BEACH', 'FlY', 'GAME'
+                    ];
+    let foundWord = '';
+    for (var i = 0; i < dictionary.length; i++) {
+      if (this.checkWord(dictionary[i])) {
+        foundWord += dictionary[i] + '\n';
+      }
+    }
+    // console.log(foundWord);
+    if (foundWord.length === 0) {
+      console.log('No word found');
+    } else {
+
+      console.log(foundWord.substr(0, foundWord.length - 1));
+    }
+  }
+
+  checkIndex(index_i, index_j, obj) {
+    for (var i in obj) {
+      if (obj[i][0] === index_i && obj[i][1] === index_j) {
+        return true;
+      }
+    }
+    return false;
   }
 
   // clone(data) {
@@ -152,5 +197,5 @@ class Boggle {
 
 
 var boggle = new Boggle();
-// boggle.generateBoard(4, 4);
-boggle.checkWord()
+boggle.generateBoard(10, 10);
+boggle.testCase()
